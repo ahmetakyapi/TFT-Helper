73c94d46c89fb804d6b3747bd493616f4c02f0c1
ahmetakyapi <ahmetakyapii@gmail.com>
2021-06-25 16:56:39 +0300

    Create Homepage.js

---
+++ src/components/Home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../Navbar/Navbar";
+
+function Home() {
+    return (
+        <Container>
+            <Navbar/>
+         <MainPage>
+             <img src="https://cdnportal.mobalytics.gg/production/2021/04/bd8b6ba0-tft_reckoning_cheatsheet_final.jpg" alt="tft photo"/>
+         </MainPage>
+        </Container>
+
+    )
+}
+const Container = styled.div `
+`
+
+const MainPage = styled.div `
+  height:85vh;
+  width: 100vh;
+  padding-top: 75px;
+  justify-content: space-between;
+  background-size: 500px;
+  display: inline-flex; 
+  
+`
+
+export default Home
