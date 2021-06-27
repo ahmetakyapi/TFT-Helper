e8749c9728c7dadf57de0111a47310e72cc58a65
ahmetakyapi <ahmetakyapii@gmail.com>
2021-06-28 00:16:37 +0300

    Champions listing the page.

--- src/components/Champions/Champions.js
+++ src/components/Champions/Champions.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 import Navbar from "../Navbar/Navbar";
+import styled from "styled-components";
+import ChampionsData from "../champions.json";
 
-function Champions() {
-    return (
-        <div>
-            <Navbar/>
-         <h1>Champions</h1>
-        </div>
-    )
+class Champions extends Component {
+    render() {
+        return (
+            <div className="ui grid container">
+                <Navbar/>
+                <Champs>
+                    {ChampionsData.map((championDetail) => {
+                        return <div>
+                            <h2>{championDetail.title} </h2>
+                            <img src={championDetail.image} alt=""/>
+                            <p>{championDetail.costs}</p>
+                        </div>
+                    })}
+                </Champs>
+            </div>
+        )
+    }
 }
-
+ const Champs = styled.div `
+     display: flex;
+     flex-direction: row;
+     padding: 6rem;
+     
+   
+   div {
+     width: 200px;
+   }
+   img {
+     max-width: 400px;
+   }
+   
+ `
 export default Champions
