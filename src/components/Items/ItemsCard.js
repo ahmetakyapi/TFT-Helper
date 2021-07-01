56d1788b6409ff16a732157a7632367aca02f7ab
ahmetakyapi <ahmetakyapii@gmail.com>
2021-07-01 16:11:17 +0300

    Complete Items.

--- src/components/Items/ItemsCard.js
+++ src/components/Items/ItemsCard.js
@@ -1,16 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 const Card = ({ data, cardIndex }) => {
     return (
-        <div>
+        <Components>
             {data[cardIndex].map(item => (
-                <div className="card">
+                <Content>
                     <p>{item.title}</p>
-                    <p>{item.name}</p>
-                </div>
+                    {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+                    <img src={item.image} alt="item image"/>
+                </Content>
             ))}
-        </div>
+        </Components>
     );
 };
-
+const Components = styled.div `
+  padding-left: 30rem;
+`
+const Content = styled.div `
+    p{
+      color: blanchedalmond;
+      text-align: center;
+      padding-bottom: 2rem;
+      padding-left: 2rem;
+      font-size: 2.2rem;
+      font-weight: 600;
+    }
+`
 export default Card;
--- src/components/Items/ItemsData.js
+++ src/components/Items/ItemsData.js
@@ -1,20 +1,68 @@
+import BfSword from '../../assets/ItemsComponents/BF Sword Components.png'
+import LargeRod from '../../assets/ItemsComponents/Large Rod Components.png'
+import RecurveBow from '../../assets/ItemsComponents/Recurve Bow Components.png'
+import ChainVest from '../../assets/ItemsComponents/Chain Vest Components.png'
+import GiantsBelt from '../../assets/ItemsComponents/Giant Belt Components.png'
+import NegatronCloak from '../../assets/ItemsComponents/Negatron Cloak Components.png'
+import SparringGloves from '../../assets/ItemsComponents/Sparring Gloves Components.png'
+import TearoftheGoddess from '../../assets/ItemsComponents/Tear of the Goddess Components.png'
+import Spatula from '../../assets/ItemsComponents/Spatula Components.png'
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
     [
         {
-            title: "This is first card",
-            name: "First Card"
+            title: "Bf Sword Components",
+            image: [BfSword]
         }
     ],
     [
         {
-            title: "This is second card",
-            name: "Second Card"
+            title: "Large Rod Components",
+            image: [LargeRod]
         }
     ],
     [
         {
-            title: "This is third card",
-            name: "Third card"
+            title: "Recurse Bow Components",
+            image: [RecurveBow]
         }
-    ]
+    ],
+
+    [
+        {
+            title: "Chain Vest Components",
+            image: [ChainVest],
+        }
+    ],
+    [
+        {
+            title: "Giant's Belt Components",
+            image: [GiantsBelt]
+        }
+    ],
+    [
+        {
+            title: "Negatron Cloak Components",
+            image: [NegatronCloak]
+        }
+    ],
+    [
+        {
+            title: "Sparring Gloves Components",
+            image: [SparringGloves]
+        }
+    ],
+    [
+        {
+            title: "Tear of the Goddess Components",
+            image: [TearoftheGoddess]
+        }
+    ],
+    [
+        {
+            title: "Spatula Components",
+            image: [Spatula]
+        }
+    ],
 ];
